3b8595057d9ff50af86a973fe03bfec0db9b5f00
Bowie <pbatista@gmail.com>
2024-05-06 20:43:39 -0300

    exercices

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,21 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._jobTitle = "student";
+        job1._company = "BYU";
+        job1._startYear = 2019;
+        job1._endYear = 2027;
+        Job job2 = new Job();
+        job2._jobTitle = "assistant";
+        job2._company = "DB1";
+        job2._startYear = 2022;
+        job2._endYear = 2023;
+        Resume myResume = new Resume();
+        myResume._name = "Guilherme Prates";
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+        myResume.Display();
     }
 }
